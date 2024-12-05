bcae6411dd6cdc1dff8bdac6269364456d72bb0f
anaiel <an.leclabart@gmail.com>
2024-12-05 14:32:03 +0100

    add crude login to hide boucherie only functionalities

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	// [TODO] Replace with actual login, very crude implementation in the mean time
+	$: boucherieLogin = false;
+	onMount(() => {
+		if (localStorage.getItem('boucherie-login')) {
+			boucherieLogin = true;
+		}
+	});
+</script>
+
 Outils disponibles :
+
 <ul>
-	<li><a href="/training-formatter">Formatter les trainings</a></li>
+	{#if boucherieLogin}<li><a href="/training-formatter">Formatter les trainings</a></li>{/if}
 	<li><a href="/jammer-heatmap">Jammer heatmap</a></li>
 </ul>
